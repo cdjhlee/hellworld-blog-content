0ef866fe455193f9c3a6e91e94428a037e5eb71e
cdjhlee <cdjhlee@gmail.com>
2016-10-01 13:49:26 +0900

    update example source code

--- http2/http2_express.js
+++ http2/http2_express.js
@@ -1,10 +1,9 @@
 var fs = require('fs');
 var spdy = require('spdy');
+var app = require('./app');
 var options = {
   key: fs.readFileSync('../common/self.key'),
   cert: fs.readFileSync('../common/self.crt')
 };
 
-spdy.createServer(options, function(request, response) {
-  response.end('Hello world!');
-}).listen(8080);
+spdy.createServer(options, app).listen(8080);
